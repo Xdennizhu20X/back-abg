bc75947b4085b71d792d119a48a966f6180e4211
ClickNvite <clicknvite1@gmail.com>
2025-06-18 10:26:56 -0500

    correccion postgress

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     dialect: 'postgres',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
     pool: {
@@ -22,6 +22,7 @@ const sequelize = new Sequelize(
     }
   }
 );
+
 
 // Función para sincronizar la base de datos
 const syncDatabase = async (force = false) => {
